8f99da688be1bb48969ab75d5a9bc98e780dc23b
manisha-13-code <manishachauhan5469@gmail.com>
2025-06-14 21:32:50 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
