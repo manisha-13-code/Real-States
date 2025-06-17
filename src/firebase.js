5595918d9d797c739a91840e1dbc756abaade3e4
manisha-13-code <manishachauhan5469@gmail.com>
2025-06-17 19:02:09 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-5df4c.firebaseapp.com",
+  projectId: "mern-estate-5df4c",
+  storageBucket: "mern-estate-5df4c.firebasestorage.app",
+  messagingSenderId: "149190881106",
+  appId: "1:149190881106:web:199f1576df124edaa6d48b",
+  measurementId: "G-7GW1367NQN"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
